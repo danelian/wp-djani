ade7f0223b862e34744ccd19c301bf2b1bf2fd9d
danelian <141317269+danelian@users.noreply.github.com>
2024-03-20 00:56:56 +0400

    register post types

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ if (!function_exists('danelian_setup')) {
       'style',
     ));
     // Добавление миниатюр для постов
-    add_theme_support('post-thumbnails', array('post'));
+    add_theme_support('post-thumbnails', array('post', 'news', 'blog', 'articles'));
   }
   add_action('after_setup_theme', 'danelian_setup');
 }
@@ -73,3 +73,98 @@ function delete_intermediate_image_sizes( $sizes ){
 		'2048x2048',
 	] );
 }
+
+
+/**
+ * РЕГИСТРАЦИЯ КАСТОМНЫХ ТИПОВ ЗАПИСЕЙ
+ */
+// Регистрация кастомного типа записей "Новости"
+function custom_post_type_news() {
+    $labels = array(
+        'name'               => __('Новости'),
+        'singular_name'      => __('Новость'),
+        'menu_name'          => __('Новости'),
+        'all_items'          => __('Все новости'),
+        'add_new'            => __('Добавить новость'),
+        'add_new_item'       => __('Добавить новую новость'),
+        'edit_item'          => __('Редактировать новость'),
+        'new_item'           => __('Новая новость'),
+        'view_item'          => __('Просмотреть новость'),
+        'search_items'       => __('Искать новость'),
+        'not_found'          => __('Новости не найдены'),
+        'not_found_in_trash' => __('Новости в корзине не найдены'),
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'public'              => true,
+        'has_archive'         => true,
+        'menu_icon'           => 'dashicons-megaphone', // Иконка в меню
+        'supports'            => array('title', 'editor', 'thumbnail', 'excerpt'),
+        'rewrite'             => array('slug' => 'news'),
+    );
+
+    register_post_type('news', $args);
+}
+add_action('init', 'custom_post_type_news');
+
+// Регистрация кастомного типа записей "Блог"
+function custom_post_type_blog() {
+    $labels = array(
+        'name'               => __('Блог'),
+        'singular_name'      => __('Пост'),
+        'menu_name'          => __('Блог'),
+        'all_items'          => __('Все записи'),
+        'add_new'            => __('Добавить запись'),
+        'add_new_item'       => __('Добавить новую запись'),
+        'edit_item'          => __('Редактировать запись'),
+        'new_item'           => __('Новая запись'),
+        'view_item'          => __('Просмотреть запись'),
+        'search_items'       => __('Искать запись'),
+        'not_found'          => __('Записи не найдены'),
+        'not_found_in_trash' => __('Записи в корзине не найдены'),
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'public'              => true,
+        'has_archive'         => true,
+        'menu_icon'           => 'dashicons-admin-post', // Иконка в меню
+        'supports'            => array('title', 'editor', 'thumbnail', 'excerpt'),
+        'rewrite'             => array('slug' => 'blog'),
+    );
+
+    register_post_type('blog', $args);
+}
+add_action('init', 'custom_post_type_blog');
+
+// Регистрация кастомного типа записей "Статьи"
+function custom_post_type_articles() {
+    $labels = array(
+        'name'               => __('Статьи'),
+        'singular_name'      => __('Статья'),
+        'menu_name'          => __('Статьи'),
+        'all_items'          => __('Все статьи'),
+        'add_new'            => __('Добавить статью'),
+        'add_new_item'       => __('Добавить новую статью'),
+        'edit_item'          => __('Редактировать статью'),
+        'new_item'           => __('Новая статья'),
+        'view_item'          => __('Просмотреть статью'),
+        'search_items'       => __('Искать статью'),
+        'not_found'          => __('Статьи не найдены'),
+        'not_found_in_trash' => __('Статьи в корзине не найдены'),
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'public'              => true,
+        'has_archive'         => true,
+        'menu_icon'           => 'dashicons-media-document', // Иконка в меню
+        'supports'            => array('title', 'editor', 'thumbnail', 'excerpt'),
+        'rewrite'             => array('slug' => 'articles'),
+    );
+
+    register_post_type('articles', $args);
+}
+add_action('init', 'custom_post_type_articles');
+?>
